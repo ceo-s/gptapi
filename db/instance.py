9b1e6e4b21374a20a9af46ab6be552f64fc075d5
ceo-s <alex_s_2002@mail.ru>
2023-08-26 02:16:25 +0300

    LMAO! Fixed. Anyway i changed the password

--- db/instance.py
+++ db/instance.py
@@ -1,15 +1,18 @@
 from sqlalchemy.orm import declarative_base
-
 from sqlalchemy.engine import URL
 from sqlalchemy.ext.asyncio import create_async_engine
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv(".env.db")
 
 postgres_url = URL.create(
     drivername="postgresql+asyncpg",
-    username="alex",
-    password="280177",
-    host="localhost",
+    username=getenv("USERNAME"),
+    password=getenv("PASSWORD"),
+    host=getenv("HOST"),
     port=5432,
-    database="gptapi"
+    database=getenv("DATABASE"),
 )
 
 BASE = declarative_base()
